8667e9ae197baf76a8e65cc3529a6b9c8ef807eb
PhyoeTheingi Khin <phyokhin869cu@gmail.com>
2024-05-27 11:02:51 +0630

    [Completed] RestClient

---
+++ ConsoleApp2/EFCoreExamples/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotNetTrainingBatch3.ConsoleApp.Models;
+using System.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetTrainingBatch3.ConsoleApp.EFCoreExamples
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+            {
+
+                DataSource = "DESKTOP-4FNB1J3\\MSSQLSERVER1",
+                InitialCatalog = "TestDb",
+                UserID = "sa",
+                Password = "phyo@123",
+                TrustServerCertificate = true
+            };
+            optionsBuilder.UseSqlServer(sqlConnectionStringBuilder.ConnectionString);
+        }
+
+        public DbSet<BlogModel> Blogs { get; set; }
+    }
+}
---
+++ ConsoleApp2/Models/BlogModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetTrainingBatch3.ConsoleApp.Models
+{
+    [Table("Tbl_Blog")]
+    public class BlogModel
+    {
+        [Key]
+        //[Column("BlogId")]
+        public int BlogId { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogAuthor { get; set; }
+        public string BlogContent { get; set; }
+    }
+
+    public class JsonPlaceholderModel
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+    }
+
+}
