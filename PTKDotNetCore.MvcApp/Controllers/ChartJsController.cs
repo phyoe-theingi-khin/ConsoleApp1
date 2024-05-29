816061d737d9be049e6a74c0ef72c237b648a9c3
PhyoeTheingi Khin <phyokhin869cu@gmail.com>
2024-05-29 14:07:25 +0630

    Complete mvc

---
+++ PTKDotNetCore.MvcApp/Controllers/ChartJsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PTKDotNetCore.MvcApp.Controllers
+{
+    public class ChartJsController : Controller
+    {
+        public IActionResult BarChart()
+        {
+            return View();
+        }
+    }
+}
