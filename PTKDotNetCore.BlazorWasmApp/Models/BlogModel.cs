42fece1d9f5f11e6f6c7a075b132416234f44dc3
PhyoeTheingi Khin <phyokhin869cu@gmail.com>
2024-06-18 08:19:17 +0630

    [Complete]BlazorWasmApp

---
+++ PTKDotNetCore.BlazorWasmApp/Models/BlogModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PTKDotNetCore.BlazorWasmApp.Models;
+
+
+public class BlogModel
+{
+
+    public string BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
+}
---
+++ PTKDotNetCore.Wasmapp2/Models/BlogModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PTKDotNetCore.WasmApp2.Models;
+
+
+public class BlogModel
+{
+
+    public int BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
+}
